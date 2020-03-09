47675a8fa1202721b019f6dc4ac2e1eb51599c92
Adam Priest <sepone229@gmail.com>
2020-03-09 22:57:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,10 +12,9 @@ end
 
 def assign_rooms(speakers)
   [1, 2, 3, 4, 5, 6, 7].each_with_index do |room_num, index|
-    puts "Hello, #{speakers}! You'll be assigned to room #{room_num}!"
   if ((index) %  1 == 0) 
   end
-  return speakers
+  return puts "Hello, #{speakers}! You'll be assigned to room #{room_num}!"
 end 
 end
 
