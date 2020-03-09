c5d2a9f09f578445b0fd712be671810a00569cdc
Adam Priest <sepone229@gmail.com>
2020-03-09 23:00:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,17 +2,18 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(speakers)
   badges = []
-  attendees.each do |badge|
+  speakers.each do |badge|
     badges << "Hello, my name is #{badge}."
   end
   return badges
 end
 
 def assign_rooms(speakers)
+  room_assignment = []
   [1, 2, 3, 4, 5, 6, 7].each_with_index do |room_num, index|
-    room_assignment = "Hello, #{speakers}! You'll be assigned to room #{room_num}!"
+    room_assignment << "Hello, #{speakers}! You'll be assigned to room #{room_num}!"
   if ((index) %  1 == 0) 
   end
   return room_assignment
