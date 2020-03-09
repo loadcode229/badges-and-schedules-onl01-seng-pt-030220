e9a4d21e8371210e1d302a64cf2b86ce46304de1
Adam Priest <sepone229@gmail.com>
2020-03-09 23:24:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(speakers)
   room_assignment = []
   speakers.each_with_index { |speaker, index| 
-  room_assignment << ("Hello, #{speakers}! You'll be assigned to room #{index + 1}!") 
+  room_assignment << ("Hello, #{speaker}! You'll be assigned to room #{index + 1}!") 
   }
 end
   return room_assignment
