651bbe3d8162a60ece81359ca2d6b38e9ebf7e3b
Adam Priest <sepone229@gmail.com>
2020-03-09 22:24:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  badges = []
+  speakers.each do |badge|
+    badges << "Hello, my name is #{badge}."
+  end
+  return badges
+end
